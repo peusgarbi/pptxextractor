ed4bf636ead2598831a1c2aa6567c75763d24d29
Pedro Sgarbi <peusgarbi@gmail.com>
2024-12-10 12:34:27 -0300

    feature(better-button)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
 	import type { GetTextFromPPTX } from "$lib";
 	import { getTextFromPPTX } from "$lib";
+	import { slide } from "svelte/transition";
 
 	let files: FileList | undefined = $state();
 	let filesText: GetTextFromPPTX | undefined = $state();
 </script>
 
 <main class="mx-auto p-2">
-	<div class="mx-auto rounded-md bg-gray-200 p-4 drop-shadow-sm">
+	<div class="flex flex-col gap-2 rounded-md bg-gray-200 p-4 drop-shadow-sm">
 		<label for="avatar">Escolha seus arquivos pdf:</label>
 		<input bind:files multiple={true} id="avatar" name="avatar" type="file" accept=".pptx,.ppt" />
 		{#if files}
 			<button
+				transition:slide
+				class="mx-auto rounded-md bg-red-200 px-3 py-2 drop-shadow-lg transition-colors hover:bg-red-100"
 				onclick={async () => {
 					if (files) {
 						filesText = await getTextFromPPTX(files[0]);
